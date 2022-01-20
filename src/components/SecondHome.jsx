6e64ee57f5312cb7662b23382e7734207ad6a6eb
Charlotte Menard <charlotte-menard_student2021@wilder.school>
2022-01-20 15:29:03 +0100

    Add context - different element for page orga & page invited

--- src/components/SecondHome.jsx
+++ src/components/SecondHome.jsx
@@ -1,24 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import CartCard from "./CartCard";
 import styles from './style/FirstHome.module.css';
+import CartProductsContext from '../contexts/cartProducts';
 
 
 export default function SecondHome() {
 
-  const [menuCursor, setMenuCursor] = useState(1);
+  const { cartProducts, setCartProducts } = useContext(CartProductsContext);
+
+  const [menuCursor, setMenuCursor] = useState(3);
 
   const handleCursor = (cursor) => {
     setMenuCursor(cursor);
   };
-
-  const [cartProducts, setCartProducts] = useState([{
-      "id": 1,
-      "name": "Peinture intérieur Blanc Satin Murs & Plafonds Bicouche Acrylique 10L - 100m² - Blanc",
-      "productRef": "ME10486098",
-      "prix": 49,
-      "vendeur": "Renaulac",
-      "imgUrl": "https://cdn.manomano.com/peinture-interieur-blanc-satin-murs-plafonds-bicouche-acrylique-10l-100m-renaulac-blanc-P-4063946-10806872_1.jpg"
-    }])
 
   return (
     <div className={styles.mainContainer}>
@@ -28,9 +22,9 @@ export default function SecondHome() {
         <h4>Continuer mes achats</h4>
       </div>
       <div className={styles.onglets}>
-        <button className={styles.ongletActif} onClick={() => handleCursor(1)} style={ menuCursor===1 ? { color: `#0c193a` } : { color: `#505971` }}>Panier ({cartProducts.length})</button>
+        <button className={styles.ongletActif} onClick={() => handleCursor(1)} style={ menuCursor===1 ? { color: `#0c193a` } : { color: `#505971` }}>Panier</button>
         <button onClick={() => handleCursor(2)} style={ menuCursor===2 ? { color: `#0c193a` } : { color: `#505971` }}>Mis de côté</button>
-        <button onClick={() => handleCursor(3)} style={ menuCursor===3 ? { color: `#0c193a` } : { color: `#505971` }}>Participatif</button>
+        <button onClick={() => handleCursor(3)} style={ menuCursor===3 ? { color: `#0c193a` } : { color: `#505971` }}>Participatif ({cartProducts.length})</button>
       </div>
       <div className={styles.cursorLine}>
         <div className={styles.cursor1} 
