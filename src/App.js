1aff669c0b611ddf2fdf2ae68ffe6ecceb918472
Matthieu Lemarchand <lemarchand.matth@gmail.com>
2022-01-20 17:13:02 +0100

    merge Charlotte

--- src/App.js
+++ src/App.js
@@ -13,16 +13,11 @@ function App() {
   return (
     <div className="App">
       <Routes>
-<<<<<<< HEAD
-        <Route exact path="/orga" element={<FirstHome />}></Route>
-        <Route exact path="/invited" element={<SecondHome />}></Route>
-=======
         <Route path="/orga" element={<FirstHome />}></Route>
         <Route path="/invited" element={<SecondHome />}></Route>
         <Route path="/listsorga" element={<SharedListsOrga />}></Route>
         <Route path="/listsinvited" element={<SharedListsInvited />}></Route>
         <Route path="/mylist" element={<MyList />}></Route>
->>>>>>> a333631c53053d7e80d0a4014c0e0dcdd119d2a2
       </Routes>
     </div>
   );
