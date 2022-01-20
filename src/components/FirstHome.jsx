4961dd018401050965da5358f76f3296cf087a28
sophielepert <sophie.lepert@gmail.com>
2022-01-20 17:21:02 +0100

    style css the share part

--- src/components/FirstHome.jsx
+++ src/components/FirstHome.jsx
@@ -59,15 +59,15 @@ function HandleSendEmail(){
   return (
     <div className={styles.mainContainer}>
       <div className={styles.headerCo}></div>
-      <div className={styles.continuer}>
-        <div className={styles.LArrow}></div>
-        <h4>Continuer mes achats</h4>
-      </div>
-      <div className={styles.onglets}>
-        <button className={styles.ongletActif} onClick={() => handleCursor(1)} style={ menuCursor===1 ? { color: `#0c193a` } : { color: `#505971` }}>Panier ({cartProducts.length})</button>
-        <button onClick={() => handleCursor(2)} style={ menuCursor===2 ? { color: `#0c193a` } : { color: `#505971` }}>Mis de côté</button>
-        <button onClick={() => handleCursor(3)} style={ menuCursor===3 ? { color: `#0c193a` } : { color: `#505971` }}>Participatif</button>
-      </div>
+       <div className={styles.continuer}>
+         <div className={styles.LArrow}></div>
+         <h4>Continuer mes achats</h4>
+       </div>
+        <div className={styles.onglets}>
+          <button className={styles.ongletActif} onClick={() => handleCursor(1)} style={ menuCursor===1 ? { color: `#0c193a` } : { color: `#505971` }}>Panier ({cartProducts.length})</button>
+          <button onClick={() => handleCursor(2)} style={ menuCursor===2 ? { color: `#0c193a` } : { color: `#505971` }}>Mis de côté</button>
+          <button onClick={() => handleCursor(3)} style={ menuCursor===3 ? { color: `#0c193a` } : { color: `#505971` }}>Participatif</button>
+        </div>
       <div className={styles.cursorLine}>
         <div className={styles.cursor1} 
         style={ menuCursor===1 ? { background: `#0c193a` } : null}
@@ -83,37 +83,42 @@ function HandleSendEmail(){
         {cartProducts && cartProducts.map((produit, menuCursor, handleCursor) => 
           <CartCard key={produit.id} produit={produit} menuCursor={menuCursor} handleCursor={handleCursor}/>
         )}
-      </div>
-      <div>
-      <Button color="danger" onClick={OpenModal}>Partager mon panier</Button>
-      <Modal isOpen={open} toggle={() => setOpen(false)}>
-        <ModalHeader>
-          Partage ton panier avec tes amis
-        </ModalHeader>
-        <ModalBody>
-          <label for='start'> Date des travaux</label>
-          <input type='date' id='start' name='workStart' min='2022-01-19' max='2032-01-19' onChange={(e) => setDate(e.target.value)} />
-          <label for="email"> Renseigne l'email de tes amis :</label>
-          <input type="email" id="email1" onChange={(e) => setEmail1(e.target.value)} pattern=".+@globex.com" size="30" required />
-          <button onClick={handleDisplayNewFriend}>+</button>
-          <div style={displayFriend}>
-          <input  type="email" id="email2" pattern=".+@globex.com" size="30" required />
+      
+        <div className={styles.buttonHome}>
+          <div className={styles.buttonIntroPresentation}>
+            <p className={styles.buttonIntroText}>Besoin d'un coup de main pour finaliser vos achats ? Demandez l'avis de vos amis en cliquant ci-dessous ! </p>
           </div>
-        </ModalBody>
-        <ModalFooter>
-          <button onClick={HandleSendEmail}>Send</button>
-        </ModalFooter>
-      </Modal>
-      </div>
-      <div className={styles.cartPrice}>
-        <div className={styles.cartSecondContainer}>
-          <div className={styles.cartText}>
-            <h3>Total du panier</h3>
-            <p>Frais de livraison calculés à l'étape suivante</p>
-          </div>
-          <h2>305 €</h2>
+          <Button className={styles.buttonShare} onClick={OpenModal}>Partager mon panier</Button>
+          <Modal isOpen={open} toggle={() => setOpen(false)}>
+          <ModalHeader style={{ color:'#0C193A', fontSize:'0.8em', textAlign:'center' }}>
+             Partage ton panier avec tes amis
+          </ModalHeader>
+          <ModalBody>
+           <label for='start'> Date des travaux </label>
+              <input type='date' id='start' name='workStart' min='2022-01-19' max='2032-01-19' onChange={(e) => setDate(e.target.value)} />
+            <label for="email"> Renseigne l'email de tes amis :</label>
+              <input type="email" id="email1" onChange={(e) => setEmail1(e.target.value)} pattern=".+@globex.com" size="30" required />
+           <button onClick={handleDisplayNewFriend}>+</button>
+           <div style={displayFriend}>
+              <input  type="email" id="email2" pattern=".+@globex.com" size="30" required />
+           </div>
+         </ModalBody>
+          <ModalFooter>
+           <button onClick={HandleSendEmail}>Send</button>
+          </ModalFooter>
+          </Modal>
         </div>
-        <button className={styles.cartButton}>Passer à la livraison</button>
+
+       <div className={styles.cartPrice}>
+         <div className={styles.cartSecondContainer}>
+           <div className={styles.cartText}>
+             <h3>Total du panier</h3>
+             <p>Frais de livraison calculés à l'étape suivante</p>
+           </div>
+            <h2>305 €</h2>
+          </div>
+         <button className={styles.cartButton}>Passer à la livraison</button>
+       </div>
       </div>
     </div>
   )
