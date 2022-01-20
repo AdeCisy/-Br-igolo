2935842f26de4e66c081a5614485b824220d4685
Eti-Fromentin <fromentin.etienne@gmail.com>
2022-01-20 10:24:12 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
-<<<<<<< Updated upstream
-=======
 import RecommandationCard from './components/bandeau';
 // import FirstHome from './components/FirstHome';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-     
-          Learn React
-
-=======
   <RecommandationCard />
->>>>>>> Stashed changes
     </div>
   );
 }
--- src/components/bandeau.jsx
+++ src/components/bandeau.jsx
@@ -5,10 +5,12 @@ import styles from './style/bandeau.module.css';
 function RecommandationCard() {
   return <div class={styles.recoCardContainer}>
       <div>
-        <img />
+        <img href="https://cdn.manomano.com/images/images_products/4298710/L/8831942_1.jpg"  alt=''/>
       </div>
       <div className={styles.contentContainer}>
-        <div className={styles.shortTitle}></div>
+        <div className={styles.shortTitle}>
+        SAVY - Rouleau anti-goutte avec rallonge - 180 mm
+        </div>
         <div className={styles.rating}></div>
         <div className={styles.price}></div>
       </div>
